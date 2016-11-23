6742ce1f85c4bd0348fe2bea9f2460823df59a9f
Veronika <40212551@live.napier.ac.uk>
2016-11-23 03:52:25 +0000

    adding  logout test, incorrect login test

--- test.py
+++ test.py
@@ -26,6 +26,26 @@ class FlaskTestCase(unittest.TestCase):
         )
         self.assertIn(b'You were just logged in', response.data)
 
+    #ensure that login behaves INcorrectly given the correct credentials
+    def test_incorrect_login(self):
+        tester = app.test_client(self)
+        response = tester.post(
+            '/login',
+            data=dict(username="wrong", password="admin"),
+            follow_redirects=True
+        )
+        self.assertIn(b'Invalid credentials. Please try again.', response.data)
+
+    #ensure that logout page loads correctly
+    def test_logout(self):
+        tester = app.test_client(self)
+        tester.post(
+            '/login',
+            data=dict(username="admin", password="admin"),
+            follow_redirects=True
+        )
+        self.assertIn(b'You were just logged out', response.data)
+
 
 if __name__ == '__main__':
     unittest.main()
