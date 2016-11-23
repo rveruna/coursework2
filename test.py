dc41dce6c80d667a7a28fc931d587e88d3802a5d
Veronika <40212551@live.napier.ac.uk>
2016-11-23 04:07:22 +0000

    adding test for post

--- test.py
+++ test.py
@@ -53,6 +53,16 @@ class FlaskTestCase(unittest.TestCase):
         response = tester.get('/', follow_redirects=True)
         self.assertTrue(b'You need to login first.' in response.data)
 
+    #ensure that post appear on post page
+    def test_post_show_up(self):
+        tester = app.test_client(self)
+        response = tester.post(
+            '/login',
+            data=dict(username="admin", password="admin"),
+            follow_redirects=True
+        )
+        self.assertIn(b'Hello from', response.data)
+
 
 
 if __name__ == '__main__':
