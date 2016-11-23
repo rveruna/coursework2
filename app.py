17ad3e7059c23733b8a1868354c1b45f4cbe91ea
Veronika <40212551@live.napier.ac.uk>
2016-11-23 00:12:35 +0000

    welcome route

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Hello world"
 
+@app.route('/welcome')
+def welcome():
+    return_template("welcome.html")
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=5000)
