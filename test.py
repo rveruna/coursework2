b5f67656dbdc431f22393a5aad7ae20672f1795f
Veronika <40212551@live.napier.ac.uk>
2016-11-23 03:25:17 +0000

    typo

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ class FlaskTestCase(unittest.TestCase):
     def test_login_page_loads(self):
         tester = app.test_client(self)
         response = tester.get('/login', content_type='html/text')
-        self.assertTrue(b'Please login in response.data)
+        self.assertTrue(b'Please login' in response.data)
 
 if __name__ == '__main__':
     unittest.main()
