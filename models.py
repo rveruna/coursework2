daead09473b73c47ec29074d6e1303986f3aa89e
Veronika <40212551@live.napier.ac.uk>
2016-11-25 18:55:14 +0000

    typo

--- models.py
+++ models.py
@@ -35,4 +35,4 @@ class User(db.Model):
         self.password = bcrypt.generate_password_hash(password)
 
     def __repr__(self):
-        return <'name{}'.format(self.name)
+        return '<name{}'.format(self.name)
